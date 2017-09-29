73452efad4d8091c0f976009ee2300f29e22ff6e
Bruno Vieira <mail@bmpvieira.com>
2017-09-29 16:47:33 +0100

    Fix syntax issues in nix.sh and add result to .gitignore

--- nix.sh
+++ nix.sh
@@ -3,7 +3,7 @@
 # Usage: source ./nix.sh
 
 function docker-load-nix() {
-  if [[ -n $(cat /proc/version | grep Microsoft) ]]; then
+  if grep -q Microsoft /proc/version; then
     DIR=$(pwd | sed 's|/mnt/\(.\)|\1:|' | sed 's|/|\\|g')
   else
     DIR=$(pwd)
@@ -17,23 +17,23 @@ function docker-load-nix() {
     ARGS=""
   fi
 
-  docker run $ARGS -v $DIR:/data $IMAGE /bin/sh
+  docker run $ARGS -v "$DIR:/data" "$IMAGE" /bin/sh
 }
 
 function nix() {
   case $1 in
     shell) nix-shell;;
-    install) nix-env -f $(pwd) -iA $2;;
-    build) nix-build $(pwd) -A $2;;
+    install) nix-env -f "$(pwd)" -iA "$2";;
+    build) nix-build "$(pwd)" -A "$2";;
     docker)
-      nix-build $(pwd) -A dockerTar --argstr pkg $2 && 
+      nix-build "$(pwd)" -A dockerTar --argstr pkg "$2" && 
       docker-load-nix interactive
     ;;
     singularity)
-      nix-build $(pwd) -A dockerTar --argstr pkg $2 && 
+      nix-build "$(pwd)" -A dockerTar --argstr pkg "$2" && 
       docker-load-nix &&
       NAME=$(docker ps -l --format "{{.Image}}" | sed 's|:|_|')
-      docker export $(docker ps -lq) | gzip > $NAME.tar.gz
+      docker export "$(docker ps -lq)" | gzip > "$NAME.tar.gz"
     ;;
     * ) echo "Options: shell, install, build, docker, singularity" ;;
   esac
