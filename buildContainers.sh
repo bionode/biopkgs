80d393fdcfb004d82ce13fc67fe45a12a8f90f3e
Bruno Vieira <mail@bmpvieira.com>
2017-10-31 16:00:03 +0000

    Add .travis.yml and fix buildContainers.sh

--- buildContainers.sh
+++ buildContainers.sh
@@ -1,13 +1,16 @@
+#! /bin/bash
 source ./nixez.sh
+repo=bionode
 nix-env -f . -qaPA 'customPkgs' | awk '{print $2}' | while read pkg; do
   echo "Checking if $pkg needs to be pushed to Docker Hubs"
   pkgName=${pkg%%-*}
   pkgVersion=${pkg##*-}
-  url="https://registry.hub.docker.com/v2/repositories/bionode/$pkgName/tags/"
+  url="https://registry.hub.docker.com/v2/repositories/$repo/$pkgName/tags/"
   if ! curl -s $url | jq '."results"[]["name"]' 2> /dev/null | grep -q $pkgVersion ; then
     echo "Building Docker image for $pkg"
     nixez docker $pkgName
     echo "Pushing $pkg to Docker Hub"
-    docker push $pkgName:$pkgVersion
+    docker tag $repo/$pkgName:$pkgVersion $repo/$pkgName:latest
+    docker push $repo/$pkgName
   fi
 done
