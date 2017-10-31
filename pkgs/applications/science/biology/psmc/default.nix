a36b8073faab53a177acbefdde377d2681563057
Bruno Vieira <mail@bmpvieira.com>
2017-10-31 13:07:28 +0000

    Comment most packages, in preparation for Travis

--- pkgs/applications/science/biology/psmc/default.nix
+++ pkgs/applications/science/biology/psmc/default.nix
@@ -1,9 +1,9 @@
 { stdenv, fetchFromGitHub, zlib }:
 
 stdenv.mkDerivation rec {
-  name = "psmc-${version}";
   baseName = "psmc";
   version = "e5f7df5"; # Git commit from January 21, 2016
+  name = "${baseName}-${version}";
 
   src = fetchFromGitHub {
     owner = "lh3";
@@ -20,22 +20,6 @@ stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/bin
     cp psmc $out/bin/
-    cp utils/avg.pl $out/bin/
-    cp utils/calD $out/bin/
-    cp utils/cntcpg $out/bin/
-    cp utils/ctime_plot.pl $out/bin/
-    cp utils/dec2ctime.pl $out/bin/
-    cp utils/dec2img.js $out/bin/
-    cp utils/decode2bed.pl $out/bin/
-    cp utils/fq2psmcfa $out/bin/
-    cp utils/history2ms.pl $out/bin/
-    cp utils/ms2psmcfa.pl $out/bin/
-    cp utils/mutDiff $out/bin/
-    cp utils/pcnt_bezier.lua $out/bin/
-    cp utils/psmc2history.pl $out/bin/
-    cp utils/psmc_plot.pl $out/bin/
-    cp utils/psmc_trunc.pl $out/bin/
-    cp utils/splitfa $out/bin/
   '';
 
   meta = with stdenv.lib; {
--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -17,26 +17,27 @@ let
   callPackage = pkgs.lib.callPackageWith (pkgs // pkgs.xlibs // self);
 
   customPkgs = {
-    shell = (import shellDir).env;
-    aspera = callPackage ../tools/networking/aspera {};
-    bbcp = callPackage ../tools/networking/bbcp {};
-    fastqc = callPackage ../applications/science/biology/fastqc {};
-    kmc = callPackage ../applications/science/biology/kmc {};
-    ncbi-vdb = callPackage ../applications/science/biology/ncbi-vdb {};
-    nextflow = callPackage ../applications/science/misc/nextflow {};
-    ngs = callPackage ../applications/science/biology/ngs {};
-    seqlib = callPackage ../applications/science/biology/seqlib {};
-    seqtk = callPackage ../applications/science/biology/seqtk {};
-    sra-tools = callPackage ../applications/science/biology/sra-tools {};
+    # aspera = callPackage ../tools/networking/aspera {};
+    # bbcp = callPackage ../tools/networking/bbcp {};
+    # fastqc = callPackage ../applications/science/biology/fastqc {};
+    # kmc = callPackage ../applications/science/biology/kmc {};
+    # ncbi-vdb = callPackage ../applications/science/biology/ncbi-vdb {};
+    # nextflow = callPackage ../applications/science/misc/nextflow {};
+    # ngs = callPackage ../applications/science/biology/ngs {};
+    # seqlib = callPackage ../applications/science/biology/seqlib {};
+    # seqtk = callPackage ../applications/science/biology/seqtk {};
+    # sra-tools = callPackage ../applications/science/biology/sra-tools {};
     psmc = callPackage ../applications/science/biology/psmc {};
-    trimmomatic = callPackage ../applications/science/biology/trimmomatic {};
-    tsunami-udp = callPackage ../tools/networking/tsunami-udp {};
-    udpcast = callPackage ../tools/networking/udpcast {};
-    udr = callPackage ../tools/networking/udr {};
-    nodejs = hiPrio nodejs-6_x;
-    nodePackages_6_x = callPackage ../development/node-packages/default-v6.nix {
-      nodejs = pkgs.nodejs-6_x;
-    };
+    # trimmomatic = callPackage ../applications/science/biology/trimmomatic {};
+    # tsunami-udp = callPackage ../tools/networking/tsunami-udp {};
+    # udpcast = callPackage ../tools/networking/udpcast {};
+    # udr = callPackage ../tools/networking/udr {};
+    # nodejs = hiPrio nodejs-6_x;
+    # nodePackages_6_x = callPackage ../development/node-packages/default-v6.nix {
+      # nodejs = pkgs.nodejs-6_x;
+    # };
+  };
+
   customEnvs = {
     shell = (import shellDir).env;
   };
