415ee7da264f79a40e934d20d7aaa167f0efc783
Bruno Vieira <mail@bmpvieira.com>
2017-11-29 15:15:24 +0000

    Also get rid of Perl (and tests) in bcftools because of Singularity

--- pkgs/applications/science/biology/bcftools/default.nix
+++ pkgs/applications/science/biology/bcftools/default.nix
@@ -3,14 +3,13 @@
 stdenv.mkDerivation rec {
   name = "${pname}-${version}";
   pname = "bcftools";
-  version = "1.6";
+  upstreamVersion = "1.6";
+  version = "${upstreamVersion}noperl";
 
   src = fetchurl {
-    url = "https://github.com/samtools/bcftools/releases/download/${version}/${name}.tar.bz2";
+    url = "https://github.com/samtools/bcftools/releases/download/${upstreamVersion}/${pname}-${upstreamVersion}.tar.bz2";
     sha256 = "10prgmf09a13mk18840938ijqgfc9y92hfc7sa2gcv07ddri0c19";
   };
-
-  nativeBuildInputs = [ perl ];
 
   buildInputs = [ htslib zlib bzip2 lzma curl ];
 
@@ -19,16 +18,6 @@ stdenv.mkDerivation rec {
     "prefix=$(out)"
     "CC=cc"
   ];
-
-  preCheck = ''
-    patchShebangs misc/
-    patchShebangs test/
-    sed -ie 's|/bin/bash|${bash}/bin/bash|' test/test.pl
-  '';
-
-  enableParallelBuilding = true;
-
-  doCheck = true;
 
   meta = with stdenv.lib; {
     description = "Tools for manipulating BCF2/VCF/gVCF format, SNP and short indel sequence variants";
