4fd8ee26c83911896d3e2eb8d92623e02f4bf074
Bruno Vieira <mail@bmpvieira.com>
2019-01-10 20:27:19 +0000

    Update upstream nixpkgs source to latest commit to fix broken pytorch

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2,9 +2,9 @@
 
 let
   pinPkgs = import (builtins.fetchTarball {
-    name = "nixos-18.09";
-    url = "https://github.com/NixOS/nixpkgs/archive/18.09.tar.gz";
-    sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
+    name = "nixos-19.01.10";
+    url = "https://github.com/NixOS/nixpkgs/archive/539d62be3aa24d20d266187b8cbe59aaf74244cc.tar.gz";
+    sha256 = "0s1sh4lvkv9z7hs2rli31xrg819jw5qiqzmj0nmml15qkw22x60j";
   }) { inherit system; };
 
   pkgs = pinPkgs // {
